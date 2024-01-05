dccf1f369e6a6d09fbed0513f144718dbe275c7e
jhoncastro1 <jhoncastro2004eg@gmail.com>
2024-01-05 18:55:42 -0500

    start with the hospital software

---
+++ Hospital_Back/src/main/java/com/hospital/commons/helpers/HelperMapper.java
@@ -0,0 +1,8 @@
+package com.hospital.commons.helpers;
+
+import org.modelmapper.ModelMapper;
+
+public class HelperMapper {
+
+    public static ModelMapper modelMapper(){return new ModelMapper();}
+}
